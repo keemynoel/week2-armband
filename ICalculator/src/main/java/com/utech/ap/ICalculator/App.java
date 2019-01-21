b9a4470bfe4bc5e2873472c0857277763e39fde7
keemynoel <keemynoel@yahoo.com>
2019-01-21 10:58:47 -0500

    Commit

---
+++ ICalculator/src/main/java/com/utech/ap/ICalculator/App.java
@@ -0,0 +1,13 @@
+package com.utech.ap.ICalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
